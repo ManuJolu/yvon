b473fbc58c5c23617bbb7febaca76957a34eff91
emmanuelle jolu <emmanuelle.jolu.ej@gmail.com>
2016-11-21 17:31:13 +0100

    model restaurant fixed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   belongs_to :user, required: true
   has_many :meals
   has_many :orders
-  has_many :ordered_meals through: :orders
+  has_many :ordered_meals, through: :orders
 
   validates :name, presence: true
   validates :address, presence: true
