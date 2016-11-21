e68bdba36840dfb7c1825c39da7cddab43221380
emmanuelle jolu <emmanuelle.jolu.ej@gmail.com>
2016-11-21 17:28:15 +0100

    model order fixed

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :restaurant, required: true
   has_many :ordered_meals
-  has_many :meals through: :ordered_meals
+  has_many :meals, through: :ordered_meals
 end
