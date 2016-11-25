1094090fb5fb7920a856a5789ab2ea2d69a10a1b
emmanuelle jolu <emmanuelle.jolu.ej@gmail.com>
2016-11-25 11:26:23 +0100

    btn ready and delivered cool

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124164819) do
+ActiveRecord::Schema.define(version: 20161125102433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20161124164819) do
     t.datetime "ready_at"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.date     "delivered_at"
+    t.datetime "delivered_at"
     t.index ["restaurant_id"], name: "index_orders_on_restaurant_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
