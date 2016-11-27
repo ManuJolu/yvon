ca26553c498ea7f0480229ce0aa1dba365af0b17
Francois Catuhe <fcatuhe@gmail.com>
2016-11-27 21:17:43 +0100

    better sort of meals in order / receipt

--- app/bot/controllers/order_controller.rb
+++ app/bot/controllers/order_controller.rb
@@ -24,6 +24,13 @@ class OrderController
         ordered_meal.meal = Meal.find(meal_id.to_i)
         ordered_meal.quantity = quantity.to_i
       end
+      order.ordered_meals = order.ordered_meals.sort do |x, y|
+        if (x.meal.category <=> y.meal.category) != 0
+          x.meal.category <=> y.meal.category
+        else
+          x.meal.name.downcase <=> y.meal.name.downcase
+        end
+      end
       order.paid_at = Time.now
 
       if order.save
