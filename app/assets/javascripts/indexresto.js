d2295cdd41a48e0178d00fb4b5bc0f1b7e0921bf
gabriel ecalle <gabriel.ecalle@googlemail.com>
2016-11-28 16:22:11 +0100

    js ds resto/orders

---
+++ app/assets/javascripts/indexresto.js
@@ -0,0 +1,24 @@
+$("#pending").click(function () {
+  $("#orderspending").toggleClass( "hidden" );
+});
+
+
+$("#pending").click(function () {
+  $("#trianglependingdown").toggleClass( "hidden" );
+});
+
+$("#pending").click(function () {
+  $("#trianglependingup").toggleClass( "hidden" );
+});
+
+$("#delivered").click(function () {
+  $("#ordersdelivered").toggleClass( "hidden" );
+});
+
+$("#delivered").click(function () {
+  $("#triangledelivereddown").toggleClass( "hidden" );
+});
+
+$("#delivered").click(function () {
+  $("#triangledeliveredup").toggleClass( "hidden" );
+});
