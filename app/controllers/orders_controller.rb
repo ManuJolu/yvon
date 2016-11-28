d93609a7ae3f9e64c5d756bc4277b0ef9b6bb582
Francois Catuhe <fcatuhe@gmail.com>
2016-11-28 17:36:40 +0100

    bug corrected, cleaned routes and controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,34 @@
 class OrdersController < ApplicationController
-  before_action :find_restaurant, only: [ :index, :show, :edit, :update ]
-  before_action :find_order, only: [ :edit, :update ]
-
-  def index
-    @orders = @restaurant.orders
-  end
-
-  def show
-  end
-
-  def edit
-  end
+  before_action :set_order, only: [:update]
+  before_action :set_restaurant, only: [:update]
 
   def update
-    if order_params[:delivered_at]
+    if order_params[:ready_at]
+      @order.ready_at = DateTime.now
+      if @order.save
+        if Rails.env.production?
+          Facebook::Messenger::Bot.deliver({
+            recipient: {
+              id: @order.user.messenger_id
+            },
+            message: {
+              text: "Hey #{@order.user.first_name}, your order at #{@order.restaurant.name} is ready for pick-up!"
+            }},
+            access_token: ENV['ACCESS_TOKEN']
+          )
+        end
+        respond_to do |format|
+          format.html { redirect_to restaurant_path(@restaurant) }
+          format.js { }
+        end
+      else
+        @meal = @restaurant.meals.new
+        respond_to do |format|
+          format.html { render 'restaurants/show' }
+          format.js
+        end
+      end
+    elsif order_params[:delivered_at]
       @order.update(delivered_at: Time.now)
       if @order.save
         if Rails.env.production?
@@ -38,66 +53,22 @@ class OrdersController < ApplicationController
         end
       else
         @meal = @restaurant.meals.new
-        @orders = @restaurant.orders.persisted
         respond_to do |format|
           format.html { render 'restaurants/show' }
           format.js { }
         end
-      end
-    elsif order_params[:ready_at]
-      @order.update(ready_at: Time.now)
-      if @order.save
-        if Rails.env.production?
-          Facebook::Messenger::Bot.deliver({
-            recipient: {
-              id: @order.user.messenger_id
-            },
-            message: {
-              text: "Hey #{@order.user.first_name}, your order at #{@order.restaurant.name} is ready for pick-up!"
-            }},
-            access_token: ENV['ACCESS_TOKEN']
-          )
-        end
-        respond_to do |format|
-          format.html { redirect_to restaurant_path(@restaurant) }
-          format.js { }
-        end
-      else
-        @meal = @restaurant.meals.new
-        @orders = @restaurant.orders.persisted
-        respond_to do |format|
-          format.html { render 'restaurants/show' }
-          format.js
-        end
-      end
-    end
-
-
-
-
-    @order.update(order_params)
-    if @order.save
-      @orders = @restaurant.orders.all
-      respond_to do |format|
-        format.html { redirect_to restaurant_path(@restaurant) }
-        format.js { }
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to restaurant_path(@restaurant) }
-        format.js
       end
     end
   end
 
   private
 
-  def find_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+  def set_order
+    @order = Order.find(params[:id])
   end
 
-  def find_order
-    @order = Order.find(params[:id])
+  def set_restaurant
+    @restaurant = @order.restaurant
   end
 
   def order_params
