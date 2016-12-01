03ff33938f8329bb4935a2e37551f1a874b70e09
Francois Catuhe <fcatuhe@gmail.com>
2016-12-01 18:36:28 +0100

    only active meals in bot

--- app/bot/controllers/meal_controller.rb
+++ app/bot/controllers/meal_controller.rb
@@ -15,7 +15,7 @@ class MealController
 
   def index(postback, params = {})
     restaurant = Restaurant.find(params[:restaurant_id])
-    meals = restaurant.meals.where(category: params[:category]).limit(9)
+    meals = restaurant.meals.active.where(category: params[:category]).limit(9)
     next_category = Meal.categories.key(Meal.categories[params[:category]] + 1)
     @view.index(postback, meals.decorate, current_category: params[:category], next_category: next_category)
   end
