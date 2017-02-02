fbaba536deaaef021d9d2498076e0e464aac4045
emmanuelle jolu <emmanuelle.jolu.ej@gmail.com>
2017-02-02 15:27:45 +0100

    i18n configuré

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,21 @@ Rails.application.routes.draw do
   mount Attachinary::Engine, at: 'attachinary'
   mount Facebook::Messenger::Server, at: 'bot'
 
-  root to: 'pages#home'
-  resources :users, only: [:show]
-  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update] do
-    member do
-      patch '/duty/:state' => "restaurants#duty"
-    end
-    resources :orders, only: [] do
-      collection do
-        get 'pending'
-      end
-    end
-    resources :meals, only: [:create, :update]
-  end
-  resources :orders, only: [:update]
-end
 
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    resources :users, only: [:show]
+    resources :restaurants, only: [:index, :show, :new, :create, :edit, :update] do
+      member do
+        patch '/duty/:state' => "restaurants#duty"
+      end
+      resources :orders, only: [] do
+        collection do
+          get 'pending'
+        end
+      end
+      resources :meals, only: [:create, :update]
+    end
+    resources :orders, only: [:update]
+  end
+end
