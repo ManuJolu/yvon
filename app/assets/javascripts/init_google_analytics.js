67448202cd82a914c3d92366996cddc1d39a78c2
Francois Catuhe <fcatuhe@gmail.com>
2017-02-10 14:18:57 +0100

    small fix GA

--- app/assets/javascripts/init_google_analytics.js
+++ app/assets/javascripts/init_google_analytics.js
@@ -1,4 +1,4 @@
-$(document).addEventListener("turbolinks:load", function(event) {
+document.addEventListener("turbolinks:load", function(event) {
   if (typeof ga === "function") {
     ga("set", "location", event.data.url);
     return ga("send", "pageview");
