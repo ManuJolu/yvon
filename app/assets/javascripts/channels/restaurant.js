52ef7b2d64684243e561780ba318714b4925e83f
Francois Catuhe <fcatuhe@gmail.com>
2017-02-14 08:44:25 +0100

    fix javascript document ready + google analytics

--- app/assets/javascripts/channels/restaurant.js
+++ app/assets/javascripts/channels/restaurant.js
@@ -1,10 +1,12 @@
-if ($(".orders.index").length > 0) {
-  App.orders = App.cable.subscriptions.create({ channel: "RestaurantChannel", restaurant_id: restaurantId }, {
-    received: function(data) {
-      $.ajax({
-        method: "get",
-        url: '/restaurants/'+ restaurantId + '/orders/refresh/'
-      });
-    }
-  });
-};
+$(document).ready(function() {
+  if ($(".orders.index").length > 0) {
+    App.orders = App.cable.subscriptions.create({ channel: "RestaurantChannel", restaurant_id: restaurantId }, {
+      received: function(data) {
+        $.ajax({
+          method: "get",
+          url: '/restaurants/'+ restaurantId + '/orders/refresh/'
+        });
+      }
+    });
+  };
+});
--- app/assets/javascripts/navbar_wagon_fixed.js
+++ app/assets/javascripts/navbar_wagon_fixed.js
@@ -1,3 +1,5 @@
-$(document).on('scroll', function (e) {
+$(document).ready(function() {
+  $(document).on('scroll', function (e) {
     $('.background-custom').css('opacity', ($(document).scrollTop() / 500));
+  });
 });
