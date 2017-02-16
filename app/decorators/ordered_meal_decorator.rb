8b74af79ff8e6f706a706a373cd95bf85f3dea09
Francois Catuhe <fcatuhe@gmail.com>
2017-02-16 17:31:04 +0100

    improve style orders index

--- app/decorators/ordered_meal_decorator.rb
+++ app/decorators/ordered_meal_decorator.rb
@@ -3,8 +3,8 @@ class OrderedMealDecorator < Draper::Decorator
   decorates_association :meal
 
   def to_s
-    str = "#{object.quantity} x #{object.meal.name}"
+    str = "#{object.quantity} <span>&times;</span> #{object.meal.name}"
     str += " - #{object.option.name}" if object.option
-    str
+    str.html_safe
   end
 end
