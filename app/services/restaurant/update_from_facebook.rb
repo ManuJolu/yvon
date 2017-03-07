c39ad8f370c71e30f2524ae0d5db87a3e74b4b0a
Francois Catuhe <fcatuhe@gmail.com>
2017-03-07 17:34:35 +0100

    keep name and address in facebook update

--- app/services/restaurant/update_from_facebook.rb
+++ app/services/restaurant/update_from_facebook.rb
@@ -15,14 +15,14 @@ class Restaurant::UpdateFromFacebook
       restaurant_data_json = RestClient.get("https://graph.facebook.com/v2.8/#{page_ref}?fields=name,about,description,price_range,fan_count,overall_star_rating,rating_count,location&access_token=#{restaurant.user.token}")
       restaurant_data = JSON.parse restaurant_data_json
       restaurant.fb_page_id = restaurant_data['id']
-      restaurant.name = restaurant_data['name']
+      restaurant.name ||= restaurant_data['name']
       restaurant.about = restaurant_data['about'] || restaurant_data['description']
       restaurant.description = restaurant_data['description']
       restaurant.fb_price_range = restaurant_data['price_range']
       restaurant.fb_fan_count = restaurant_data['fan_count']
       restaurant.fb_overall_star_rating = restaurant_data['overall_star_rating']
       restaurant.fb_rating_count = restaurant_data['rating_count']
-      restaurant.address = "#{restaurant_data['location']['street']}, #{restaurant_data['location']['zip']} #{restaurant_data['location']['city']}, #{restaurant_data['location']['country']}"
+      restaurant.address ||= "#{restaurant_data['location']['street']}, #{restaurant_data['location']['zip']} #{restaurant_data['location']['city']}, #{restaurant_data['location']['country']}"
       return false unless restaurant.save
 
       restaurant_picture_data_json = RestClient.get("https://graph.facebook.com/v2.8/#{page_ref}/picture?width=480&redirect=0&access_token=#{restaurant.user.token}")
