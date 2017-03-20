d61f58751822d46ca6f06b2681cde52fe1b0c063
Francois Catuhe <fcatuhe@gmail.com>
2017-03-20 23:55:53 +0100

    remove rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/rails-admin', as: 'rails_admin'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   mount Attachinary::Engine, at: 'attachinary'
