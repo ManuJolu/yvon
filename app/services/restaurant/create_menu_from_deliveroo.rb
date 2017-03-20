0d3a5302009eb01d420002e51b5ccdcb193a1a66
Francois Catuhe <fcatuhe@gmail.com>
2017-03-20 14:56:21 +0100

    correct price in imports

--- app/services/restaurant/create_menu_from_deliveroo.rb
+++ app/services/restaurant/create_menu_from_deliveroo.rb
@@ -15,7 +15,7 @@ class Restaurant::CreateMenuFromDeliveroo
           meal_category.meals.create(
             name: menu_item.css('.list-item-title').text.strip,
             description: menu_item.css('.list-item-description').text.strip,
-            price: menu_item.css('.item-price').text.strip.to_f
+            price: menu_item.css('.item-price').text.strip.gsub(',', '.').to_f
           )
         end
       end
