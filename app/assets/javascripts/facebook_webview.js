ef642d4f410bdb04b11244e580d3ee669d1b1c1e
Francois Catuhe <fcatuhe@gmail.com>
2017-03-21 17:58:07 +0100

    change connection depending on website or webview

---
+++ app/assets/javascripts/facebook_webview.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  if ($(".sessions.new").length > 0) {
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'Messenger'));
+
+    window.extAsyncInit = function() {
+      MessengerExtensions.getSupportedFeatures(function success() {
+        $('#webviewLoginForm').show();
+      }, function error(err) {
+        $('#websiteLoginForm').show();
+      });
+    };
+  };
+
+  if ($(".users.show").length > 0) {
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'Messenger'));
+  };
+});
+
+
