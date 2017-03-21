cb5a933c9c2e8749395925772bad0afd1a31df65
Francois Catuhe <fcatuhe@gmail.com>
2017-03-21 01:19:25 +0100

    Revert "reverting"
    This reverts commit 84fc0809956816a375537bde5462035681912894.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/rails-admin', as: 'rails_admin'
+  ActiveAdmin.routes(self)
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   mount Attachinary::Engine, at: 'attachinary'
