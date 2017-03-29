ee27dcc0eb6aa7d87e17e8fc429f36f4fddd7cae
Francois Catuhe <fcatuhe@gmail.com>
2017-03-29 21:01:23 +0200

    order of RestaurantCategories

--- app/models/restaurant_category.rb
+++ app/models/restaurant_category.rb
@@ -2,4 +2,6 @@ class RestaurantCategory < ApplicationRecord
   has_many :restaurants, dependent: :restrict_with_exception
 
   validates :name, presence: true
+
+  default_scope { order(name: :asc) }
 end
