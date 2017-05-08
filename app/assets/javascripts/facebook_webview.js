3aff00efe89fa28874845f99acd4f8936c2ec8d6
Francois Catuhe <fcatuhe@gmail.com>
2017-05-08 10:56:07 +0200

    fix login button, but regression because of FB getSupportedFeatures change

--- app/assets/javascripts/facebook_webview.js
+++ app/assets/javascripts/facebook_webview.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
-  if ($(".sessions.new").length > 0) {
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'Messenger'));
-
-    window.extAsyncInit = function() {
-      MessengerExtensions.getSupportedFeatures(function success(result) {
-        console.log(result.supported_features)
-        alert(result)
-        alert(result.supported_features)
-        if (result.supported_features[0] === 'context') {
-          $('#webviewLoginForm').show();
-        } else {
-          $('#websiteLoginForm').show();
-        }
-      }, function error(err) {
-        $('#websiteLoginForm').show();
-        alert('fail')
-      });
-    };
-  };
-
   if ($(".users.show").length > 0) {
     (function(d, s, id){
       var js, fjs = d.getElementsByTagName(s)[0];
